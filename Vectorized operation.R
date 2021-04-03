08c6f9a0d99e677c19ee34f341a39bc255b7b8eb
Ummar Ahmed <ummarahmed001@gmail.com>
2021-04-03 06:09:11 +0500

    Create Vectorized operation.R
    Vectorized operation

---
+++ Vectorized operation.R
@@ -0,0 +1,29 @@
+#Vectorized operation
+
+#on vectors
+x <- 1:4; y <- 11:14
+
+x<3
+
+y<21
+
+x+y
+
+x-y
+
+x*y
+
+x/y
+
+x^y
+
+
+#Operations on Matrics
+x <- matrix(1:4, 2,2); y <- matrix(rep(10,4), 2,2)
+x
+y
+
+x+y
+x-y
+x*y
+x%*%y
